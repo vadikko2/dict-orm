dead56fe552b3c2634aa2b98ef48a3e3c38258d1
Vadim Kozyrevskii <vadikko2@mail.ru>
2022-03-04 16:33:51 +0300

    Update orm_models.py

--- orm_models.py
+++ orm_models.py
@@ -10,8 +10,13 @@ class BaseModel:
     __slots__ = ('__tablename__',)
 
     @classmethod
-    def get_object(cls, controller: Type[BaseController]) -> Type[BaseObject]:
+    def get_object(
+            cls,
+            controller: Type[BaseController],
+            cls_name=BaseObject.__class__.__name__
+    ) -> Type[BaseObject]:
         """Return BaseObject instance, generated for described data Model"""
         BaseObject.__controller__ = controller
         BaseObject.__model__ = cls
+        BaseObject.__name__ = cls_name
         return BaseObject
