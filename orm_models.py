5d4b360d427771b3290359044e619fa93bea88c5
Vadim Kozyrevskii <vadikko2@mail.ru>
2022-03-04 15:13:36 +0300

    Update orm_models.py

--- orm_models.py
+++ orm_models.py
@@ -1,6 +1,17 @@
 """Data models classes"""
+from typing import Type
+
+from orm_controllers import BaseController
+from orm_objects import BaseObject
 
 
 class BaseModel:
     """Describe data model"""
     __slots__ = ('__tablename__',)
+
+    @classmethod
+    def get_object(cls, controller: Type[BaseController]) -> Type[BaseObject]:
+        """Return BaseObject instance, generated for described data Model"""
+        BaseObject.__controller__ = controller
+        BaseObject.__model__ = cls
+        return BaseObject
