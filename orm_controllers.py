0bbc9f06493bf533044042a741f3ff35f05c8124
Vadim Kozyrevskii <vadikko2@mail.ru>
2022-03-04 16:20:27 +0300

    Update orm_controllers.py

--- orm_controllers.py
+++ orm_controllers.py
@@ -26,15 +26,19 @@ class BaseController(ABC):
         """Select Query"""
         raise NotImplementedError
 
-    def bulk_insert(self, rows: list):
+    def insert(self, row):
+        """Insert Query"""
+        raise NotImplementedError
+
+    def bulk_insert(self, rows: Iterable):
         """Bulk Insert Query"""
         raise NotImplementedError
 
-    def update(self, new_data: dict):
+    def update(self, row, filters):
         """Update Query"""
         raise NotImplementedError
 
-    def delete(self):
+    def delete(self, filters):
         """Delete Query"""
         raise NotImplementedError
 
@@ -45,6 +49,10 @@ class DictBaseController(BaseController):
     @staticmethod
     def connect(db: Dict):
         BaseController.__database__ = db
+
+    @staticmethod
+    def disconnect():
+        BaseController.__database__ = None
 
     def select(self, attrs: Iterable[Type], filters: Iterable[Tuple[Type, Callable]]):
         """Select data from db by filters"""
@@ -69,3 +77,19 @@ class DictBaseController(BaseController):
                 if key in attr_names:
                     row_data[key] = value
             yield self.object_class(**row_data)
+
+    def insert(self, row):
+        """Insert BaseObject to database"""
+        self.__database__[self.model_class.__table_name__].append(row.as_dict())
+
+    def bulk_insert(self, rows: Iterable):
+        """Bulk Insert BaseObjects to database"""
+        self.__database__[self.model_class.__table_name__] += [row.as_dict() for row in rows]
+
+    def update(self, row, filters: Iterable[Tuple[Type, Callable]]):
+        """Update BaseObject attributes in database"""
+        pass
+
+    def delete(self, filters: Iterable[Tuple[Type, Callable]]):
+        """Delete BaseObject from database"""
+        pass
