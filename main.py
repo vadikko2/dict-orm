457d6322177c8da1e7f66158d08aaa33b26d1523
Vadim Kozyrevskii <vadikko2@mail.ru>
2022-03-04 16:41:04 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,147 +1,95 @@
-"""
-This is super simple dict ORM. 
-This GIST shows how to use property for describe data models.
-"""
-from typing import Type
+from abc import ABC
+from typing import Iterable, Callable, Tuple, Dict
 
-from orm_controllers import DictBaseController
-from orm_models import BaseModel
-from orm_objects import BaseObject
-from orm_types import Str, Age
-
-db = {
-    'employees': [
-        {
-            'name': 'Vadim',
-            'lastname': 'Kozyrevskii',
-            'position': 'Python Programmer',
-            'age': 26
-        },
-        {
-            'name': 'Alex',
-            'lastname': 'Alyinic',
-            'position': 'C++/Qt Programmer',
-            'age': 32
-        },
-        {
-            'name': 'Alexei',
-            'lastname': 'Vasiliev',
-            'position': 'C/C++ Programmer',
-            'age': 40
-        }
-    ]
-}
+from orm_types import Type
 
 
-class EmployeeModel(BaseModel):
-    __table_name__ = "employees"
+class BaseController(ABC):
+    """Interface for data base queries."""
+    __slots__ = ('__database__',)
 
-    name = Str(name='name')
-    lastname = Str(name='lastname')
-    age = Age(name='age')
-    position = Str(name='position')
+    def __init__(self, object_class):
+        self.object_class = object_class
+        self.model_class = self.object_class.__model__
+
+    @staticmethod
+    def connect():
+        """Connect to database method"""
+        raise NotImplementedError
+
+    @staticmethod
+    def disconnect():
+        """Disconnect method"""
+        raise NotImplementedError
+
+    def select(self, attrs, filters):
+        """Select Query"""
+        raise NotImplementedError
+
+    def insert(self, row):
+        """Insert Query"""
+        raise NotImplementedError
+
+    def bulk_insert(self, rows: Iterable):
+        """Bulk Insert Query"""
+        raise NotImplementedError
+
+    def update(self, row, filters):
+        """Update Query"""
+        raise NotImplementedError
+
+    def delete(self, filters):
+        """Delete Query"""
+        raise NotImplementedError
 
 
-if __name__ == '__main__':
-    # Connect to db
-    DictBaseController.connect(db=db)
-    # Create Object class
-    EmployeeObject = EmployeeModel.get_object(
-        controller=DictBaseController,
-        cls_name='EmployeeObject'
-    )  # type: Type[BaseObject]
+class DictController(BaseController):
+    """Dictionary Data Base controller"""
 
-    # ------------------ SELECT FRO DB ------------------
+    @staticmethod
+    def connect(db: Dict):
+        BaseController.__database__ = db
 
-    ems = EmployeeObject.objects.select(
-        attrs=(
-            EmployeeModel.name, EmployeeModel.lastname,
-            EmployeeModel.age, EmployeeModel.position
-        ),
-        filters=[
-            (EmployeeModel.age, lambda x: x > 30),
-            (EmployeeModel.position, lambda x: 'C++' in x)
-        ]
-    )
+    @staticmethod
+    def disconnect():
+        BaseController.__database__ = None
 
-    for em in ems:
-        print(em)
+    def select(self, attrs: Iterable[Type], filters: Iterable[Tuple[Type, Callable]]):
+        """Select data from db by filters"""
+        table_data = self.__database__.get(self.model_class.__table_name__)
+        attr_names = {attr.name for attr in attrs}
+        value_filters = {attr.name: filter_ for attr, filter_ in filters}
 
-    print('\n')
+        for row in table_data:
 
-    # Result:
-    # <EmployeeObject: (position='C++/Qt Programmer', age=32, name='Alex', lastname='Alyinic')>
-    # <EmployeeObject: (position='C/C++ Programmer', age=40, name='Alexei', lastname='Vasiliev')>
+            check_filter = True
 
-    # ------------------ CREATE NEW EMPLOYEE ------------------
+            for attr, filter_func in value_filters.items():
+                value = row.get(attr)
+                check_filter = filter_func(value) if value else False
+                if not check_filter: break
 
-    test_em = EmployeeObject(
-        name='TestName', lastname='TestLastName', age=42,
-        position='TestPosition'
-    )  # type: BaseObject
+            if not check_filter:
+                continue
 
-    print(test_em.as_dict())
-    print('\n')
+            row_data = {}
+            for key, value in row.items():
+                if key in attr_names:
+                    row_data[key] = value
+            yield self.object_class(**row_data)
 
-    # Result: {'name': 'TestName', 'position': 'TestPosition', 'lastname': 'TestLastName', 'age': 42}
+    def insert(self, row):
+        """Insert BaseObject to database"""
+        self.__database__[self.model_class.__table_name__].append(row.as_dict())
 
-    # ------------------ INSERT AND AFTER THAT SELECT NEW EMPLOYEE FROM DB ------------------
+    def bulk_insert(self, rows: Iterable):
+        """Bulk Insert BaseObjects to database"""
+        self.__database__[self.model_class.__table_name__] += [row.as_dict() for row in rows]
 
-    EmployeeObject.objects.insert(row=test_em)
+    def update(self, row, filters: Iterable[Tuple[Type, Callable]]):
+        """Update BaseObject attributes in database"""
+        pass
 
-    test_ems = EmployeeObject.objects.select(
-        attrs=(
-            EmployeeModel.name, EmployeeModel.lastname,
-            EmployeeModel.age, EmployeeModel.position
-        ),
-        filters=[
-            (EmployeeModel.name, lambda x: x == test_em.name.value)
-        ]
-    )
-
-    for em in test_ems:
-        print(em)
-    print('\n')
-
-    # Result: <EmployeeObject: (position='TestPosition', age=42, lastname='TestLastName', name='TestName')>
-
-    # ------------------ BULK INSERT AND AFTER THAT SELECT NEW EMPLOYEES FROM DB ------------------
-
-    bi_test_ems = [
-        EmployeeObject(
-            name='TestName%s' % i, lastname='TestLastName%s' % i,
-            age=42 + i, position='TestPosition%s' % i)
-        for i in range(10)
-    ]
-
-    EmployeeObject.objects.bulk_insert(rows=bi_test_ems)
-
-    gotten_test_ems = EmployeeObject.objects.select(
-        attrs=(
-            EmployeeModel.name, EmployeeModel.lastname,
-            EmployeeModel.age, EmployeeModel.position
-        ),
-        filters=[
-            (EmployeeModel.name, lambda x: x.startswith('TestName'))
-        ]
-    )
-
-    for em in gotten_test_ems:
-        print(em)
-    print('\n')
-
-    # Result:
-    # <EmployeeObject: (position='TestPosition', age=42, lastname='TestLastName', name='TestName')>
-    # <EmployeeObject: (position='TestPosition0', age=42, lastname='TestLastName0', name='TestName0')>
-    # <EmployeeObject: (position='TestPosition1', age=43, lastname='TestLastName1', name='TestName1')>
-    # <EmployeeObject: (position='TestPosition2', age=44, lastname='TestLastName2', name='TestName2')>
-    # <EmployeeObject: (position='TestPosition3', age=45, lastname='TestLastName3', name='TestName3')>
-    # <EmployeeObject: (position='TestPosition4', age=46, lastname='TestLastName4', name='TestName4')>
-    # <EmployeeObject: (position='TestPosition5', age=47, lastname='TestLastName5', name='TestName5')>
-    # <EmployeeObject: (position='TestPosition6', age=48, lastname='TestLastName6', name='TestName6')>
-    # <EmployeeObject: (position='TestPosition7', age=49, lastname='TestLastName7', name='TestName7')>
-    # <EmployeeObject: (position='TestPosition8', age=50, lastname='TestLastName8', name='TestName8')>
-    # <EmployeeObject: (position='TestPosition9', age=51, lastname='TestLastName9', name='TestName9')>
-
-    # Disconnect from db
-    DictBaseController.disconnect()
+    def delete(self, filters: Iterable[Tuple[Type, Callable]]):
+        """Delete BaseObject from database"""
+        pass
